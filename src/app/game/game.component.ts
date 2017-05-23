006ec1908e86e63322d1c48caccadbca7bc1cc5d
grapgrap <fussaep@gmail.com>
2017-05-23 13:20:05 +0900

    지난 3개월 평가 없을시 0으로 초기화

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { isUndefined } from 'util';
 import { GameService } from '../shared/service/game.service';
+import 'rxjs/add/observable/never';
 
 @Component({
     selector: 'app-game',
@@ -12,11 +13,10 @@ import { GameService } from '../shared/service/game.service';
 export class GameComponent implements OnInit {
     game: any;
     tagList$: Observable<any>;
-    gameRateAVG: any;
-    collaborateFilterRate: number;
+    gameRateAVG$: Observable<any>; gameRateAVG: any;
+    collaborateFilterRate$: Observable<number>; collaborateFilterRate: number;
 
     constructor (private gameService: GameService, private route: ActivatedRoute) {
-        this.collaborateFilterRate = -1;
     }
 
     ngOnInit () {
@@ -25,21 +25,27 @@ export class GameComponent implements OnInit {
                 .subscribe(data => {
                     this.game = data;
                     this.tagList$ = this.gameService.getTagByGameId(this.game.id);
-                    this.gameService.getGameRateAVGById(this.game.id)
-                        .subscribe(data => {
+                    this.gameRateAVG$ = this.gameService.getGameRateAVGById(this.game.id)
+                        .map(data => {
                             if ( isUndefined(data) ) {
-                                this.gameRateAVG = {
+                                return {
                                     rate_avg: 0,
                                     rate_count: 0,
                                     rate_avg_3_month: 0,
                                     rate_count_3_month: 0
                                 };
                             } else {
-                                this.gameRateAVG = data;
+                                if ( data.rate_avg_3_month === null ) {
+                                    data.rate_avg_3_month = 0;
+                                }
+                                if ( data.rate_count_3_month === null ) {
+                                    data.rate_count_3_month = 0;
+                                }
+                                return data;
                             }
                         });
-                    this.gameService.getCollaborateFilter(this.game.id, 30)
-                        .subscribe(data => data < 5 ? this.collaborateFilterRate = data : this.collaborateFilterRate = 5);
+                    this.collaborateFilterRate$ = this.gameService.getCollaborateFilter(this.game.id, 30)
+                        .map(data => data < 5 ? data : 5);
                 });
         });
     }
