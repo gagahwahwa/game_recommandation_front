3494d84b0d178c70aedb74235fb7df2f8bc30471
grapgrap <fussaep@gmail.com>
2017-12-27 11:26:05 +0900

    beta 1.0.2 feat: make sign up form

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
+  signUpFormGroup: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder, private router: Router) {
   }
 
+  ngOnInit() {
+    this.signUpFormGroup = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  signup() {
+    this.router.navigateByUrl('/sign-in/init-data');
+  }
 }
--- src/app/sign-in/sign-in.module.ts
+++ src/app/sign-in/sign-in.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { SignInRoutingModule } from './sign-in-routing.module';
 import { SignInComponent } from './sign-in.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
+    FlexLayoutModule,
+    ReactiveFormsModule,
     SignInRoutingModule
   ],
   declarations: [SignInComponent]
