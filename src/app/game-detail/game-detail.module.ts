b44f0e42c81d2fcb9680b20766f28d91d1c3919c
grapgrap <fussaep@gmail.com>
2017-12-27 18:11:15 +0900

    beta 1.0.3 feat: Make page about game detail

--- src/app/game-detail/game-detail.module.ts
+++ src/app/game-detail/game-detail.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { GameDetailRoutingModule } from './game-detail-routing.module';
 import { GameDetailComponent } from './game-detail.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
     CommonModule,
+    FlexLayoutModule,
     GameDetailRoutingModule
   ],
   declarations: [GameDetailComponent]
