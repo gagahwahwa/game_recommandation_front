1096c8ce5a801e39a64687eeaf6106a35a506686
grapgrap <fussaep@gmail.com>
2018-04-16 16:36:42 +0900

    update(navbar): sessionStorage에 저장된 id를 my page url에 추가

--- src/app/shared/component/navbar/navbar.component.ts
+++ src/app/shared/component/navbar/navbar.component.ts
@@ -7,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   nickname: string;
+  id: number;
 
   constructor() { }
 
   ngOnInit() {
     this.nickname = sessionStorage.getItem('nickname') || '';
+    this.id = +sessionStorage.getItem('id') || -1;
   }
 }
