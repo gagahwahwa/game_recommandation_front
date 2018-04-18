454245290fd14935ffd09c965f39bc1539a907bc
grapgrap <fussaep@gmail.com>
2018-04-18 13:21:18 +0900

    fix(login): 로그인 모듈에 import 한 카로셀 라이브러리 모듈 삭제

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,20 +2,16 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { UserService } from '../shared/service/user.service';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-import { UserService } from '../shared/service/user.service';
-
-import { Ng2CarouselamosModule } from 'ng2-carouselamos';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
     FlexLayoutModule,
-    LoginRoutingModule,
-    Ng2CarouselamosModule
+    LoginRoutingModule
   ],
   declarations: [LoginComponent],
   providers: [UserService]
