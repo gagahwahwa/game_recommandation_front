a79daff79293f1c852d0a6b45c98a3e2f17dbe36
grapgrap <fussaep@gmail.com>
2018-04-22 18:52:17 +0900

    fix(game detail): 잘못된 params 변수 명 수정

--- src/app/game-detail/game-detail.component.ts
+++ src/app/game-detail/game-detail.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { ActivatedRoute, Router } from '@angular/router';
-import { GameDetailService } from '../shared/service/game-detail.service';
-import { Route } from '@angular/compiler/src/core';
-import { Observer } from 'rxjs/Observer';
+import { ActivatedRoute } from '@angular/router';
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import { GameDetailService } from '../shared/service/game-detail.service';
 
 @Component({
   selector: 'app-game-detail',
@@ -14,17 +12,17 @@ import 'rxjs/add/operator/map';
 
 export class GameDetailComponent implements OnInit {
 
-  constructor( private gameService: GameDetailService, private route: ActivatedRoute) { // rout?
-  }
-
   recent3monthRate$: Observable<Array<any>>;
   allRate$: Observable<Array<any>>;
   userRate$: Observable<Array<any>>;
   gamedetailInfo$: Observable<Array<any>>;
   postingID: number;
 
+  constructor(private gameService: GameDetailService, private route: ActivatedRoute) { // rout?
+  }
+
   ngOnInit() {
-    this.postingID = this.route.snapshot.params.id;
+    this.postingID = this.route.snapshot.params.gameId;
 
     this.gamedetailInfo$ = this.gameService.getGameInformation(this.postingID);
     this.recent3monthRate$ = this.gameService.getGame3monthRate(this.postingID);
--- src/app/game-detail/game-detail.module.ts
+++ src/app/game-detail/game-detail.module.ts
@@ -2,11 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { NavbarModule } from '../shared/component/navbar/navbar.module';
+import { GameDetailService } from '../shared/service/game-detail.service';
 import { CommentModule } from './comment/comment.module';
 
 import { GameDetailRoutingModule } from './game-detail-routing.module';
 import { GameDetailComponent } from './game-detail.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
@@ -19,4 +19,5 @@ import { FlexLayoutModule } from '@angular/flex-layout';
   declarations: [GameDetailComponent],
   providers: [GameDetailService]
 })
-export class GameDetailModule { }
+export class GameDetailModule {
+}
