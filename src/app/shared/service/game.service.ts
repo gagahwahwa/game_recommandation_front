1380df3cf00d7933da10e8a97f906a44052ad4b5
grapgrap <fussaep@gmail.com>
2018-04-25 21:42:25 +0900

    feat(search result): 페이지 마크업 중간 점검 버전

--- src/app/shared/service/game.service.ts
+++ src/app/shared/service/game.service.ts
@@ -1,17 +1,26 @@
-import { Injectable } from '@angular/core';
-import { GlobalService } from '../store/global.service';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
+import { GlobalService } from '../store/global.service';
+
 
 @Injectable()
 export class GameService {
   private url: string;
+
   constructor(private global: GlobalService, private http: HttpClient) {
     this.url = global.url;
   }
 
   getGameListByTagList(tagList: Array<any>): Observable<Array<any>> {
     const tagIdList = tagList.map(tag => tag.id);
-    return this.http.get(this.url + `/gamelist-tag/${JSON.stringify(tagIdList)}`).map((res: any) => res.result === 'success' ? res.data : []);
+    return this.http.get(this.url + `/gamelist-tag/${JSON.stringify(tagIdList)}`)
+      .map((res: any) => res.result === 'success' ? res.data : []);
+  }
+
+  searchGame(keyword: string): Observable<Array<any>> {
+    return this.http.get(this.url + `/search?keyword=${keyword}`)
+      .map((res: any) => res.result === 'success' ? res.data : []);
   }
 }
