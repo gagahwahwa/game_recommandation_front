afec3fb104151b86e606f76ee45893bf8890870a
JoHyeonjeong <guswjd7104@naver.com>
2018-04-26 17:07:16 +0900

    commit

--- src/app/game-detail/comment/comment.component.ts
+++ src/app/game-detail/comment/comment.component.ts
@@ -12,34 +12,35 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class CommentComponent implements OnInit {
   CommentFormGroup: FormGroup;
   comments$: Observable<any>;
-  gameID: number;
+  game_id: number;
+  user_id: number;
 
   constructor(private commentService: CommentService, private fb: FormBuilder, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     // 라우터에서 gameID를 받아옴
     // 왜 못받아옴 id -> gameID 바꿔도 안되쟈냐~~~~?
-    this.gameID = this.route.snapshot.params.gameID;
-    console.log(this.gameID);
-    this.gameID = 13763 ;
+    this.game_id = this.route.snapshot.params.gameID;
+    console.log(this.game_id);
+    this.game_id = 13763 ;
+    this.user_id = Number(sessionStorage.getItem('id'));
     // formGroup 선언
     this.CommentFormGroup = this.fb.group({
-      gameID: [this.gameID],
+      id: [0],
       nickname: [''], // 작성자 닉네임
       regi_date: [''], // 댓글 등록 날짜
       rate: [0], // 평점
       comment: [''] // 댓글 내용
     });
-
-    this.comments$ = this.commentService.getComments(this.gameID);
-
+    this.comments$ = this.commentService.getComments(this.game_id);
+    console.log(this.user_id);
   }
   newComment(formGroup: FormGroup) {
     // 백엔드로 댓글 전송
     this.commentService.postComment(formGroup.value).subscribe((res: any) => {
       if ( res.result === 'success') { // 성공시
         // 페이지 리로드(?)
-        this.comments$ = this.commentService.getComments(this.gameID);
+        this.comments$ = this.commentService.getComments(this.game_id);
       }
     });
   }
