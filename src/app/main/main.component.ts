9adf6ddd2ceec524cd51f1afabad2070c34679d9
DESKTOP-B2AN77G\nes03 <1.1>
2018-04-29 00:21:11 +0900

    main css 수정, search 페이지로 라우팅 수정

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -36,7 +36,16 @@ export class MainComponent implements OnInit {
   }
 
   searchClick(form: FormGroup) {
-    this.router.navigate([`/search/${form.controls.keyword.value}`]);
+    // if value invlolve #, then split
+    const inputValue = form.controls.keyword.value;
+    let realValue = '';
+
+    if (inputValue.search('#') !== -1 ) {
+      realValue = inputValue.split('#');
+      this.router.navigate([`/search/tag/${realValue[1]}`]);
+    } else {
+      this.router.navigate([`/search/game/${form.controls.keyword.value}`]);
+    }
   }
 
 }
