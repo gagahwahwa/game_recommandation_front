7a4236d66c4bdb961187d36fbe859939fcfa53ff
grapgrap <fussaep@gmail.com>
2018-04-28 19:21:40 +0900

    update(more-rating): progress bar에

--- src/app/shared/component/game-card-container/game-card/game-card.component.ts
+++ src/app/shared/component/game-card-container/game-card/game-card.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
-import { InitDataStoreService } from '../../../../sign-up/init-data/shared/store/init-data-store.service';
 import { RateService } from '../../../service/rate.service';
 
 @Component({
@@ -10,15 +9,17 @@ import { RateService } from '../../../service/rate.service';
 })
 export class GameCardComponent implements OnInit {
   @Input() game: any;
+  @Output() rateCountChange = new EventEmitter();
   isStarRateBarShown: boolean;
   rate: number;
 
-  constructor(private rateService: RateService, private initStore: InitDataStoreService, private router: Router) {
+  constructor(private rateService: RateService, private router: Router) {
   }
 
   ngOnInit() {
     this.isStarRateBarShown = false;
   }
+
   rateChange(rate: number) {
     this.rate = rate;
     this.rateService.postRate({
@@ -26,12 +27,10 @@ export class GameCardComponent implements OnInit {
       user_id: sessionStorage.getItem('id'),
       rate: this.rate
     }).subscribe((res: any) => {
-      if (
-        this.router.url === '/sign-up/init-data/2'
-        && res.result === 'success'
-        && res.data.affectedRows === 1
-      ) {
-        this.initStore.ratingCount++;
+      if (res.result === 'success') {
+        this.rateCountChange.emit(true);
+      } else {
+        alert(res.msg);
       }
     });
   }
