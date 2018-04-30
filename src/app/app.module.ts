e865f69c1a5d769d10ab1c13acab3b5a579211bd
grapgrap <fussaep@gmail.com>
2018-04-30 17:08:57 +0900

    update(init-data): 스타일 수정

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +17,7 @@ import { GlobalService } from './shared/store/global.service';
   imports: [
     BrowserModule,
     HttpClientModule,
+    FlexLayoutModule,
     AppRoutingModule,
     NavbarModule,
     MainModule
