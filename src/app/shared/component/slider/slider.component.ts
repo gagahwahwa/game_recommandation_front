b01545172c9a7bf26995c630ec73c65e5beac0d2
grapgrap <fussaep@gmail.com>
2018-04-30 15:31:23 +0900

    feat(slider): 3초마다 다른 이미지 보여줌

--- src/app/shared/component/slider/slider.component.ts
+++ src/app/shared/component/slider/slider.component.ts
@@ -1,20 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { interval } from 'rxjs/observable/interval';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss']
 })
-export class SliderComponent implements OnInit {
+export class SliderComponent implements OnInit, OnDestroy {
   @Input() items: any[];
   selected: number;
+  subscription: Subscription;
 
   constructor(private router: Router) {
   }
 
   ngOnInit() {
     this.selected = 0;
+    this.subscription = interval(3000).subscribe((NOT_USE: number) => {
+      this.next();
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   next() {
@@ -31,5 +41,9 @@ export class SliderComponent implements OnInit {
     } else {
       this.selected--;
     }
+  }
+
+  changeCurrent(selected: number) {
+    this.selected = selected;
   }
 }
