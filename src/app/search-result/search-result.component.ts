3a90c02628b4f8532e9856f6d91850f628cdb06e
grapgrap <fussaep@gmail.com>
2018-05-28 17:33:53 +0900

    update(search): 검색바 가운데 정렬, 재 검색시 리스트 변경 update(slider): 아이템 클릭 가능하게 변경

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -1,31 +1,36 @@
-import { Component, HostListener, OnChanges, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { map, mergeMap, shareReplay } from 'rxjs/operators';
 import { GameService } from '../shared/service/game.service';
 
 @Component({
   selector: 'app-search-result',
   templateUrl: './search-result.component.html',
-  styleUrls: ['./search-result.component.scss']
+  styleUrls: ['./search-result.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SearchResultComponent implements OnInit, OnChanges {
+export class SearchResultComponent implements OnInit {
   type: string;
-  keyword: string;
+  keyword$: Observable<any>;
   searchedGames$: Observable<Array<any>>;
   currentPage: number;
+  lastScrollHeight: number;
 
   constructor(private gameService: GameService, private route: ActivatedRoute) {
   }
 
-  ngOnChanges() {
-    console.log(this.currentPage);
-  }
-
   ngOnInit() {
     this.type = this.route.snapshot.url[0].path;
-    this.keyword = this.route.snapshot.params.keyword;
-    this.searchedGames$ = this.gameService.searchGame(this.type, this.keyword);
+    this.keyword$ = this.route.params.pipe(
+      map((param: any) => param.keyword),
+      shareReplay()
+    );
+    this.searchedGames$ = this.keyword$.pipe(
+      mergeMap((keyword: string) => this.gameService.searchGame(this.type, keyword))
+    );
     this.currentPage = 1;
+    this.lastScrollHeight = 0;
   }
 
   @HostListener('window:scroll', ['$event'])
@@ -33,7 +38,11 @@ export class SearchResultComponent implements OnInit, OnChanges {
     const windowHeight = event.target.scrollingElement.clientHeight;
     const scrollableHeight = event.target.scrollingElement.scrollHeight;
     const currentHeight = event.target.scrollingElement.scrollTop;
-    if (scrollableHeight * 0.8 < currentHeight + windowHeight) {
+    if (currentHeight === 0) {
+      this.currentPage = 1;
+      this.lastScrollHeight = 0;
+    } else if (scrollableHeight - windowHeight * 0.2 < currentHeight + windowHeight && this.lastScrollHeight < currentHeight + windowHeight) {
+      this.lastScrollHeight = currentHeight + windowHeight;
       this.currentPage++;
     }
   }
