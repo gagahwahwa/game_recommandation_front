57d9186e5cfd9e58028ac60acc924af18fe42b12
grapgrap <fussaep@gmail.com>
2018-06-06 16:04:04 +0900

    update(more-rating): 무한 스크롤 추가

--- src/app/more-rating/more-rating.component.ts
+++ src/app/more-rating/more-rating.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { GameService } from '../shared/service/game.service';
 import { UserService } from '../shared/service/user.service';
@@ -11,11 +11,17 @@ import { UserService } from '../shared/service/user.service';
 export class MoreRatingComponent implements OnInit {
   gameList$: Observable<Array<any>>;
   count$: Observable<any>;
+  currentPage: number;
+  lastScrollHeight: number;
 
   constructor(private gameService: GameService, private userSerivce: UserService) {
   }
 
   ngOnInit() {
+    this.currentPage = 1;
+    this.lastScrollHeight = 0;
+
+
     this.gameList$ = this.gameService.getMoreRatingList(+sessionStorage.getItem('id'));
     this.count$ = this.userSerivce.getUserRateCount(+sessionStorage.getItem('id'));
   }
@@ -23,6 +29,20 @@ export class MoreRatingComponent implements OnInit {
   changeRateCount(isChange: boolean) {
     if (isChange) {
       this.count$ = this.userSerivce.getUserRateCount(+sessionStorage.getItem('id'));
+    }
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  next(event: any) {
+    const windowHeight = event.target.scrollingElement.clientHeight;
+    const scrollableHeight = event.target.scrollingElement.scrollHeight;
+    const currentHeight = event.target.scrollingElement.scrollTop;
+    if (currentHeight === 0) {
+      this.currentPage = 1;
+      this.lastScrollHeight = 0;
+    } else if (scrollableHeight - windowHeight * 0.2 < currentHeight + windowHeight && this.lastScrollHeight < currentHeight + windowHeight) {
+      this.lastScrollHeight = currentHeight + windowHeight;
+      this.currentPage++;
     }
   }
 }
